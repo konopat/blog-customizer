e8154d2f36468534e9eefa39ca1706795e28fde2
Roman Predein <roman@predein.com>
2024-07-16 13:42:03 +0800

    feat(articleparamsform): собрал и стилизовал форму из компонентов UI согласно макету
    BREAKING CHANGE: Теперь у нас есть настоящая форма для управления контентом на странице. Браво,
    товарищи! Впереди еще больше!

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -4,6 +4,17 @@ import { Button } from 'components/button';
 import styles from './ArticleParamsForm.module.scss';
 import { SidebarProps } from 'src/types';
 import clsx from 'clsx';
+import { Select } from '../select';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
+import { Text } from '../text';
+import {
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
 
 export const ArticleParamsForm: React.FC<SidebarProps> = ({
 	isOpen,
@@ -23,6 +34,36 @@ export const ArticleParamsForm: React.FC<SidebarProps> = ({
 				// В зависимости от состояния сайдбара меняем стили
 				className={clsx(styles.container, isOpen && styles.container_open)}>
 				<form className={styles.form}>
+					<Text as='h2' size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Select
+						title='Шрифт'
+						selected={fontFamilyOptions[0]}
+						options={fontFamilyOptions}
+					/>
+					<RadioGroup
+						name={'font-size-field'}
+						options={fontSizeOptions}
+						selected={fontSizeOptions[0]}
+						title={'Размер шрифта'}
+					/>
+					<Select
+						title='Цвет шрифта'
+						selected={fontColors[0]}
+						options={fontColors}
+					/>
+					<Separator />
+					<Select
+						title='Цвет фона'
+						selected={backgroundColors[0]}
+						options={backgroundColors}
+					/>
+					<Select
+						title='Ширина контента'
+						selected={contentWidthArr[0]}
+						options={contentWidthArr}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
